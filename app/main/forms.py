12d2913453f337368458599446defa469c54571e
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-09-14 08:19:35 +0300

    Add submit pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import Required
+
+class PitchForm(FlaskForm):
+    pitch = TextAreaField('Your Pitch', validators=[Required()])
+    # my_category = StringField('Category', validators=[Required()])
+    my_category = SelectField('Category', choices=[('Interview-Pitch','Interview Pitch'),('Product-Pitch','Product Pitch'),('Promotion-Pitch','Promotion Pitch'),('Business','Business'),('Academic','Academic'),('Political','Political'),('Technology','Technology'),('Health','Health')],validators=[Required()])
+    submit = SubmitField('Pitch!')
+
