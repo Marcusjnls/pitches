a56e676b6d7d47ace6d761333875b31c013420df
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-09-19 15:21:05 +0300

    Add configurations

--- config.py
+++ config.py
@@ -1,15 +1,59 @@
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 class Config:
-    pass
+    '''
+    Describes the general configurations
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DATABASE_PASS = os.environ.get('DATABASE_PASS')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    #Simple MDE configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
-    pass
+    '''
+    Production configuration child class
+    
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
 
 class TestConfig(Config):
-    pass
+    '''
+    Test configuration child class
+    
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    DATABASE_PASS = os.environ.get('DATABASE_PASS')
+
 
 class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
+config_options ={"production":ProdConfig,"development":DevConfig,"test":TestConfig}
 
